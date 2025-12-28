a953dadcc9a2231d13229a31372ed1e2fd98f43d
navneetsharma212006 <navneetsh21@gmail.com>
2025-12-28 20:53:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ScientificCalculator from './ScientificCalculator'
+
+const App = () => {
+  return (
+    <div>
+      <ScientificCalculator/>
+    </div>
+  )
+}
+
+export default App
